28564ea1d2644b5c07265b1204c0f7900cde4c0f
Sergey Gultyayev <gultyayev.sergey@gmail.com>
2024-03-10 16:42:07 +0100

    chore: Use simpler syntax for .scss lint-staged script

--- lint-staged/index.js
+++ lint-staged/index.js
@@ -19,7 +19,7 @@ export function addLintStaged() {
   // run linters and prettier on according files one by one
   packageObj["lint-staged"] = {
     "*.{js,ts,html}": ["eslint --fix --cache", "prettier --write"],
-    "*.{scss}": ["stylelint --fix --cache", "prettier --write"],
+    "*.scss": ["stylelint --fix --cache", "prettier --write"],
     "!*.{ts,js,html,scss,svg}": "prettier --write --ignore-unknown",
     "*.svg": "svgo",
   };
--- lint-staged/index.spec.js
+++ lint-staged/index.spec.js
@@ -41,19 +41,19 @@ describe("addLintStaged", () => {
         {
           "lint-staged": {
             "*.{js,ts,html}": ["eslint --fix --cache", "prettier --write"],
-            "*.{scss}": ["stylelint --fix --cache", "prettier --write"],
+            "*.scss": ["stylelint --fix --cache", "prettier --write"],
             "!*.{ts,js,html,scss,svg}": "prettier --write --ignore-unknown",
             "*.svg": "svgo",
           },
         },
         null,
-        2
+        2,
       ),
-      "utf8"
+      "utf8",
     );
 
     expect(logEnd).toHaveBeenCalledWith(
-      "lint-staged installed and ready to use"
+      "lint-staged installed and ready to use",
     );
   });
 
