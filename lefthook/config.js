28290e2100bae81ee78ae837b2902e8a15d09fb1
Sergey Gultyayev <gultyayev.sergey@gmail.com>
2025-12-29 14:54:46 +0100

    fix: hooks don't commit changes

--- lefthook/config.js
+++ lefthook/config.js
@@ -3,20 +3,22 @@ export const lefthookConfig = String.raw`pre-commit:
   jobs:
     - name: html_ts
       glob: '*.{html,ts}'
-      stage_fixed: true
       group:
         piped: true
         jobs:
           - run: npx eslint --fix {staged_files}
+            stage_fixed: true
           - run: npx prettier --write {staged_files}
+            stage_fixed: true
     - name: scss
       glob: '*.scss'
-      stage_fixed: true
       group:
         piped: true
         jobs:
           - run: npx stylelint --fix {staged_files}
+            stage_fixed: true
           - run: npx prettier --write {staged_files}
+            stage_fixed: true
     - name: rest
       exclude: '*.{html,ts,scss}'
       stage_fixed: true
